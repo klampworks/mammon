727563575c0eb17656d018e3fbfd3c65e7b65e36
yukipyon <wolfvbear@gmail.com>
2013-12-21 16:06:20 +0000

    Abstracted the parsing to a separate function.

--- test/hello_kitty.cpp
+++ test/hello_kitty.cpp
@@ -1,25 +1,36 @@
 #define PUGIXML_HEADER_ONLY
 #include "pugi_lib/pugixml.cpp"
 #include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> parse_path(const char *filename, const char *xpath) {
+
+	pugi::xml_document doc;
+	pugi::xml_parse_result result = doc.load_file(filename);
+
+	pugi::xpath_node_set imgs = doc.select_nodes(xpath);
+
+	std::vector<std::string> res;
+
+	for (const auto &node : imgs)
+	        res.push_back(node.node().attribute("src2").value());
+
+	return std::move(res);
+}
 
 int main() {
 
-	pugi::xml_document doc;
+	const char *filename = "input.html";
+	const char *xpath = "//img[@class='img_hid']";
 
-	pugi::xml_parse_result result = doc.load_file("input.html");
-
-	if (!result) {
-		std::cout << "Bad parse." << std::endl;
-		std::cout << "Error description: " << result.description() << "\n";
-	}
-
-	pugi::xpath_node_set imgs = doc.select_nodes("//img");
-
-	std::cout << "Images...:";
-
-	for (const auto &node : imgs) {
+	std::vector<std::string> res = parse_path(filename, xpath);
 	
-	        std::cout << " " << node.node().attribute("src").value() << std::endl;
+	for (const auto &s : res) {
+
+		std::cout << s << "\n";
 	}
 
+
+	std::cout << std::endl;
 }
