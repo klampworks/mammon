74ba3e9db325d109395d277d630a23ceb58b935b
yukipyon <wolfvbear@gmail.com>
2013-12-21 15:08:06 +0000

    Added parsing from a buffer.

--- pugi_tests/main.cpp
+++ pugi_tests/main.cpp
@@ -4,6 +4,15 @@
 
 int main() {
 
+	const char source[] = "<mesh name='sphere'><bounds>0 0 1 1</bounds></mesh>";
+	size_t size = sizeof(source);
+
+	char *buffer = new char[size];
+	memcpy(buffer, source, size);
+
 	pugi::xml_document doc;
+
+	pugi::xml_parse_result result = doc.load_buffer_inplace_own(buffer, size);
 	std::cout << "hello world" << std::endl;
+
 }
