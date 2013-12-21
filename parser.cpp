cbf40803b8144160fc7d9b466d5b69a4b861c000
yukipyon <wolfvbear@gmail.com>
2013-12-21 16:09:50 +0000

    Added a parser namespace for mammon. I thnk this is pretty much all we need in the way of a parser. At least for now.

---
+++ parser.cpp
@@ -0,0 +1,23 @@
+#define PUGIXML_HEADER_ONLY
+#include "pugi_lib/pugixml.cpp"
+#include <string>
+#include <vector>
+
+namespace parser {
+
+std::vector<std::string> parse_path(const char *filename, const char *xpath) {
+
+	pugi::xml_document doc;
+	pugi::xml_parse_result result = doc.load_file(filename);
+
+	pugi::xpath_node_set imgs = doc.select_nodes(xpath);
+
+	std::vector<std::string> res;
+
+	for (const auto &node : imgs)
+	        res.push_back(node.node().attribute("src2").value());
+
+	return std::move(res);
+}
+
+} //namespace
---
+++ parser.hpp
@@ -0,0 +1,13 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+class vector;
+class string;
+
+namespace parser {
+
+	std::vector<std::string> parse_path(const char*, const char*);
+
+}
+
+#endif
