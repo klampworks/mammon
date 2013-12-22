10c4251d0a9e9fb7f19fb78e3fa4d0e24ac64b2d
yukipyon <wolfvbear@gmail.com>
2013-12-22 11:24:54 +0000

    Added a test app with command line arguments to avoid recompiling.

--- chan/main.cpp
+++ chan/main.cpp
@@ -1,15 +1,22 @@
 #include "../parser.hpp"
 #include <iostream>
 
-int main() {
+int main(int argc, char **argv) {
+
+	if (argc < 3) {
+		std::cout << "Input xpath and attribute." << std::endl;
+		return 0;
+	}
 
 	const char *filename = "input.html";
-	const char *xpath = "//span[@class='reflink']/span/a";
-	const char *att = "href";
+	//const char *xpath = "//span[@class='reflink']/span/a";
+	const char *xpath = argv[1];
+	const char *att = argv[2];
 
-	std::vector<std::string> res = parser::parse_path(filename, xpath);
+	std::vector<std::string> res = parser::parse_path(filename, xpath, att);
 
 	for (const auto &a : res) {
-		std::cout << a << std::endl;
+		if (!a.empty())
+			std::cout << a << std::endl;
 	}
 }
