bc7b60a9aa8bc70c4e0678e47a0ef815209fefe1
yukipyon <wolfvbear@gmail.com>
2013-12-22 11:57:48 +0000

    Added convertor function to create urls  from ids.

--- chan/chan_parser.cpp
+++ chan/chan_parser.cpp
@@ -11,13 +11,29 @@ std::vector<std::string> parse_thread_ids() {
 	std::vector<std::string> ret;
 
 	//Only take the odd numbered values.
+	
 	for (unsigned i = 1; i < ids.size(); i+=2) {
 		ret.push_back(ids[i]);
 	}
+	
 
 	return ret;
-
 }
+
+std::vector<std::string> convert_id_to_url(const std::vector<std::string> &ids) {
+
+	//2374.html
+	const char *str_url = "http://desuchan.net/tech/res/";
+	const char *end_url = ".html";
+
+	std::vector<std::string> ret;
+
+	for (const auto &id : ids)
+		ret.push_back(str_url + id.substr(1) + end_url);
+
+	return std::move(ret);
+}
+
 
 int main(int argc, char **argv) {
 
