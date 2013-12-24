12005a53fdb8a31d7582d047e07c0b8fe7bde1f9
yukipyon <wolfvbear@gmail.com>
2013-12-24 15:26:49 +0000

    Added prototype for insert row.

--- chan/chan_db.hpp
+++ chan/chan_db.hpp
@@ -1,10 +1,15 @@
 #ifndef CHAN_DB_HPP
 #define CHAN_DB_HPP
 
+#include <string>
+
 namespace chan_db {
 
-void init();
-void init_table(const char *table_name);
+	void init();
+	void init_table(const char *table_name);
+	void insert_post(const char*, const std::string&, const std::string&,
+		const std::string&, const std::string&, const std::string&);
+
 
 }//namespace
 
