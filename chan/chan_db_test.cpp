1605616886da0fd847cefb65198b5c2345731d88
yukipyon <wolfvbear@gmail.com>
2013-12-24 15:38:58 +0000

    Fixed typos.

--- chan/chan_db_test.cpp
+++ chan/chan_db_test.cpp
@@ -1,9 +1,11 @@
 #include "chan_db.hpp"
+#include <string>
 
 int main() {
 	
+	const char *table_name = "desuchan";
 	chan_db::init();
-	chan_db::init_table("desuchan");
+	chan_db::init_table(table_name);
 
 	std::string 
 		thread_id("666"),
@@ -11,6 +13,9 @@ int main() {
 		board("test"),
 		text("This is a test."),
 		img("test.jpg");
+
+	chan_db::insert_post(table_name,
+		board, thread_id, post_id, img, text);
 
 	return 0;
 }
