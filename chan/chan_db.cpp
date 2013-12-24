9a3daaf51a6739410ed9b7dff93384d67d6f5f3e
yukipyon <wolfvbear@gmail.com>
2013-12-24 13:58:44 +0000

    Added check for existsing posts.

--- chan/chan_db.cpp
+++ chan/chan_db.cpp
@@ -21,4 +21,27 @@ void init_table(const char *table_name) {
 	base_db::init_table(table_name, columns);
 }
 
+void insert_post(
+	const char *table_name,
+	const std::string &board,
+	const std::string &thread_id,
+	const std::string &post_id,
+	const std::string &img,
+	const std::string &text)
+{
+	//First check to see if this post already exists.
+	std::string statment("select ROWID from " + std::string(table_name) + " where "
+		"board=? and thread_id=? and post_id=?");
+
+	const std::vector<std::string> values({board, thread_id, post_id});
+	std::string res = base_db::lookup_single_value(statment, values);
+
+	if (!res.empty())
+		//This post already exists.
+		return;
+
+
+
+}
+
 }//namespace
