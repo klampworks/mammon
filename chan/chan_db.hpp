0c3f9c18a8b719e9fc13a6fad402784e0e539057
yukipyon <wolfvbear@gmail.com>
2013-12-25 10:21:16 +0000

    Added a chan post class.

--- chan/chan_db.hpp
+++ chan/chan_db.hpp
@@ -3,6 +3,21 @@
 
 #include <string>
 
+struct chan_post {
+
+	std::string
+		board,
+		thread_id,
+		post_id,
+		img,
+		content;
+	
+	chan_post(const std::string &board_p, const std::string &thread_id_p,
+		const std::string &post_id_p, const std::string &img_p, 
+		const std::string &content_p) :
+			board(board_p), thread_id(thread_id_p), post_id(post_id_p),
+			img(img_p), content(content_p) {}
+};
 namespace chan_db {
 
 	void init();
