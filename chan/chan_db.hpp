2489a9bfc3c3f638441ecc32bac1ec9de5d1a032
yukipyon <wolfvbear@gmail.com>
2013-12-25 10:22:38 +0000

    Added rvalue referance constructor overload.

--- chan/chan_db.hpp
+++ chan/chan_db.hpp
@@ -17,6 +17,11 @@ struct chan_post {
 		const std::string &content_p) :
 			board(board_p), thread_id(thread_id_p), post_id(post_id_p),
 			img(img_p), content(content_p) {}
+
+	chan_post(const std::string &board_p, const std::string &thread_id_p,
+		std::string &&post_id_p, std::string &&img_p, std::string &&content_p) :
+			board(board_p), thread_id(thread_id_p), post_id(std::move(post_id_p)),
+			img(std::move(img_p)), content(std::move(content_p)) {}
 };
 namespace chan_db {
 
