aa0d4c1ba3acef877f44a2a215ab11f17082657f
yukipyon <wolfvbear@gmail.com>
2013-12-25 10:55:53 +0000

    Moved chan_post class to its own source file.

--- chan/chan_db.hpp
+++ chan/chan_db.hpp
@@ -3,27 +3,7 @@
 
 #include <string>
 #include <vector>
-
-struct chan_post {
-
-	std::string
-		board,
-		thread_id,
-		post_id,
-		img,
-		content;
-	
-	chan_post(const std::string &board_p, const std::string &thread_id_p,
-		const std::string &post_id_p, const std::string &img_p, 
-		const std::string &content_p) :
-			board(board_p), thread_id(thread_id_p), post_id(post_id_p),
-			img(img_p), content(content_p) {}
-
-	chan_post(const std::string &board_p, const std::string &thread_id_p,
-		std::string &&post_id_p, std::string &&img_p, std::string &&content_p) :
-			board(board_p), thread_id(thread_id_p), post_id(std::move(post_id_p)),
-			img(std::move(img_p)), content(std::move(content_p)) {}
-};
+#include "chan_post.hpp"
 
 namespace chan_db {
 
