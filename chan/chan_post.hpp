86b9c48d56ffb45f0be7af7cf4f125b8e8839bc9
yukipyon <wolfvbear@gmail.com>
2013-12-25 14:14:28 +0000

    Added url to the post class.

--- chan/chan_post.hpp
+++ chan/chan_post.hpp
@@ -8,18 +8,21 @@ struct chan_post {
 		thread_id,
 		post_id,
 		img,
+		img_url,
 		content;
 	
 	chan_post(const std::string &board_p, const std::string &thread_id_p,
 		const std::string &post_id_p, const std::string &img_p, 
-		const std::string &content_p) :
+		const std::string &img_url_p, const std::string &content_p) :
 			board(board_p), thread_id(thread_id_p), post_id(post_id_p),
-			img(img_p), content(content_p) {}
+			img(img_p), img_url(img_url_p), content(content_p) {}
 
 	chan_post(const std::string &board_p, const std::string &thread_id_p,
-		std::string &&post_id_p, std::string &&img_p, std::string &&content_p) :
+		std::string &&post_id_p, std::string &&img_p, 
+		std::string &&img_url_p, std::string &&content_p) :
 			board(board_p), thread_id(thread_id_p), post_id(std::move(post_id_p)),
-			img(std::move(img_p)), content(std::move(content_p)) {}
+			img(std::move(img_p)), img_url(std::move(img_url_p)), 
+			content(std::move(content_p)) {}
 };
 
 #endif
