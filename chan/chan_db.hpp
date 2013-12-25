f24731355d3051c30500aadd86493db719928075
yukipyon <wolfvbear@gmail.com>
2013-12-25 10:53:49 +0000

    Added insert_posts prototype.

--- chan/chan_db.hpp
+++ chan/chan_db.hpp
@@ -2,6 +2,7 @@
 #define CHAN_DB_HPP
 
 #include <string>
+#include <vector>
 
 struct chan_post {
 
@@ -23,12 +24,13 @@ struct chan_post {
 			board(board_p), thread_id(thread_id_p), post_id(std::move(post_id_p)),
 			img(std::move(img_p)), content(std::move(content_p)) {}
 };
+
 namespace chan_db {
 
 	void init();
 	void init_table(const char *table_name);
-	void insert_post(const char*, const std::string&, const std::string&,
-		const std::string&, const std::string&, const std::string&);
+	bool insert_post(const char*, chan_post post);
+	void insert_posts(const char*, std::vector<chan_post>&);
 
 
 }//namespace
