896ee3c0a9849f3dc6884ac36fccdfad9973ad2b
yukipyon <wolfvbear@gmail.com>
2013-12-26 16:13:04 +0000

    Moved what was essentially testing code to the testing source file.

--- chan/chan_parser_test.cpp
+++ chan/chan_parser_test.cpp
@@ -15,9 +15,48 @@ std::vector<std::string> convert_id_to_url(const std::vector<std::string> &ids) 
 	return std::move(ret);
 }
 
-int main() {
+void parse_list_page() {
 
 	chan_parser p;
+
+	std::ifstream ifs;
+	ifs.open("input.html");
+	std::string xml, tmp;
+
+	while(std::getline(ifs, tmp))
+		xml += tmp;
+
+	auto threads = p.parse_threads("test", xml);
+
+	for (const auto &thread: threads) {
+
+		for (const auto &post : thread) {
+
+			std::cout << "##############################" << std::endl;
+			std::cout << "Board = " << post.board << std::endl;
+			std::cout << "Thread = " << post.thread_id << std::endl;
+			std::cout << "Id = " << post.post_id << std::endl;
+			std::cout << "Image = " << post.img_url << std::endl;
+			std::cout << "Image = " << post.img<< std::endl;
+			std::cout << "Text = " << post.content << std::endl;
+			std::cout << "##############################" << std::endl;
+		}
+
+		std::cout << "##############################" << std::endl;
+		std::cout << "##############################" << std::endl;
+		std::cout << "##############################" << std::endl;
+		std::cout << "##############################" << std::endl;
+	}
+}
+
+int main() {
+
+	parse_list_page();
+	return 0;
+	chan_parser p;
+
+	//p.parse_thread_ids();
+	return 0; 
 
 	std::ifstream ifs;
 	ifs.open("input_2.html");
