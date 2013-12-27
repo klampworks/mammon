7d5f0aca5fae2ce8971e995d2c397af2f36c5518
yukipyon <wolfvbear@gmail.com>
2013-12-27 15:56:21 +0000

    Added process list page method.

--- chan/chan_driver.cpp
+++ chan/chan_driver.cpp
@@ -2,4 +2,6 @@
 #include "chan_parser.hpp"
 #include "chan_driver.hpp"
 
+void chan_driver::process_list_page(const std::string &hmtl) {
 
+}
--- chan/chan_driver.hpp
+++ chan/chan_driver.hpp
@@ -1,7 +1,9 @@
 #ifndef CHAN_DRIVER_HPP
 #define CHAN_DRIVER_HPP
 
-class chan_driver {
+struct chan_driver {
 
+	chan_parser parser;
+	void process_list_page(const std::string &xml);
 };
 #endif
