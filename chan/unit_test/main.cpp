ebb56a39632a35415b54ae7a50ccfaeac0d84821
yukipyon <wolfvbear@gmail.com>
2013-12-27 11:26:15 +0000

    Added a file to string function.

--- chan/unit_test/main.cpp
+++ chan/unit_test/main.cpp
@@ -1,7 +1,23 @@
 #define BOOST_TEST_MODULE example
 #include <boost/test/included/unit_test.hpp>
+#include "../chan_parser.hpp"
+#include <fstream>
+#include <string>
 
-BOOST_AUTO_TEST_CASE(test_case_1) {
+std::string dump_file(const char *filename) {
 
+	std::ifstream ifs;
+	ifs.open("input_2.html");
+	std::string xml, tmp;
+
+	while(std::getline(ifs, tmp))
+		xml += tmp;
+
+	return std::move(xml);
+}
+
+BOOST_AUTO_TEST_CASE(final_page) {
+
+	chan_parser p;
 	BOOST_CHECK(true);
 }
