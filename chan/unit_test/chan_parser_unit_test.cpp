5bdc0a4ef5e7e2853f5e8842376a859c7be338a3
yukipyon <wolfvbear@gmail.com>
2013-12-27 15:42:05 +0000

    Fixed failing unit test.

--- chan/unit_test/chan_parser_unit_test.cpp
+++ chan/unit_test/chan_parser_unit_test.cpp
@@ -67,13 +67,14 @@ BOOST_AUTO_TEST_CASE(check_img) {
 	chan_parser p;
 
 	const char *xml("<span class=\"filesize\">File: <a target=\"_blank\""
-	"href=\"http://desuchan.net/tech/src/1356867546959.jpg\">1356867546959.jpg</a>");
+	" href=\"http://desuchan.net/tech/src/1356867546959.jpg\">1356867546959.jpg</a>"
+	" -(<em>165.0 KiB, 636x603</em>)</span>");
 
 	const char *expected = "http://desuchan.net/tech/src/1356867546959.jpg";
 
 	pugi::xml_document doc;
 	doc.load(xml);
-	std::cout << p.parse_post_img(doc) << std::endl;
+
 	BOOST_CHECK(p.parse_post_img(doc) == expected);
 }
 
