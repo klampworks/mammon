880b0584bc83507e2362af68de9442322fe5cec1
yukipyon <wolfvbear@gmail.com>
2013-12-27 16:08:37 +0000

    Fleshed out new function.

--- chan/chan_driver.cpp
+++ chan/chan_driver.cpp
@@ -2,6 +2,23 @@
 #include "chan_parser.hpp"
 #include "chan_driver.hpp"
 
+//Given the html souce, figure out which threads need crawling.
 void chan_driver::process_list_page(const std::string &hmtl) {
 
+	//Get a list of threads with a handful of the most recent posts for each.
+	std::vector<std::vector<chan_post>> threads = parser.parse_threads(html);
+
+	for (const auto &thread : threads) {
+
+		//If the final post already exists in the db then skip this thread.
+		if (chan_db::post_exists(table_name, thread.back()))
+			break;
+
+		//Interate through the posts backwards.
+		for (int i = thread.size() - 1; i != -1; i--) {
+			
+
+		}
+
+	}
 }
--- chan/chan_driver.hpp
+++ chan/chan_driver.hpp
@@ -4,6 +4,8 @@
 struct chan_driver {
 
 	chan_parser parser;
+	const char *table_name = "desuchan";
+
 	void process_list_page(const std::string &xml);
 };
 #endif
