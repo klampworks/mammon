d2d6d27563040a6b676542448dce7e070e36b859
yukipyon <wolfvbear@gmail.com>
2013-12-27 15:49:46 +0000

    Added chan_driver, the class that will glue to parsing, database and networking code.

---
+++ chan/chan_driver.cpp
@@ -0,0 +1,5 @@
+#include "chan_db.hpp"
+#include "chan_parser.hpp"
+#include "chan_driver.hpp"
+
+
---
+++ chan/chan_driver.hpp
@@ -0,0 +1,7 @@
+#ifndef CHAN_DRIVER_HPP
+#define CHAN_DRIVER_HPP
+
+class chan_driver {
+
+};
+#endif
