bfbc382204dca08baf24deb17101102b939c370c
yukipyon <wolfvbear@gmail.com>
2013-12-28 14:10:10 +0000

    Added kyukon integration to the driver test file.

--- chan/chan_driver_test.cpp
+++ chan/chan_driver_test.cpp
@@ -1,6 +1,25 @@
 #include "chan_driver.hpp"
+#include "../kyukon/kyukon.hpp"
 
 int main() {
+
+	std::ifstream ifs;
+	ifs.open("proxies");
+
+	assert(ids.good());
+
+	std::vector<std::pair<std::string, bool>> proxies;
+	std::string tmp;
+
+	while(std::getline(ifs, tmp)) {
+		
+		assert(!tmp.empty());
+		proxies.push_back(std::pair<std::string, bool>(tmp, true));
+	}
+
+	assert(!proxies.empty());
+
+	kyukon::init(proxies);
 
 	chan_driver d;
 }
