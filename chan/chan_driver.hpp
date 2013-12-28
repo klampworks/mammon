b58251e9b571224f50e605bd0b02c7e460e37edd
yukipyon <wolfvbear@gmail.com>
2013-12-28 14:37:06 +0000

    Updated header.

--- chan/chan_driver.hpp
+++ chan/chan_driver.hpp
@@ -4,16 +4,18 @@
 #include <string>
 #include "chan_post.hpp"
 #include "chan_parser.hpp"
+#include "../kyukon/task.hpp"
 
 struct chan_driver {
 
 	chan_parser parser;
 	const char *table_name = "desuchan";
+	unsigned domain_id;
 
-	void process_list_page(const std::string &xml);
-	void grab_post_img(const chan_post &post);
-	void grab_thread(const chan_post &post);
-	void process_thread(const std::string &html);
+	void process_list_page(task *t);
+	void grab_post_img(const chan_post &post, const std::string &referer);
+	void grab_thread(const chan_post &post, const std::string &referer);
+	void process_thread(task *t);
 	chan_driver();
 	void fillup();
 };
