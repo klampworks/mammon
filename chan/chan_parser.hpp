ff0695ea3b05658a04cc51e9cf55f10c9d97048a
yukipyon <wolfvbear@gmail.com>
2013-12-28 11:03:19 +0000

    Fixed werid formatting.

--- chan/chan_parser.hpp
+++ chan/chan_parser.hpp
@@ -1,20 +1,20 @@
 #ifndef CHAN_PARSER_HPP
-    #define CHAN_PARSER_HPP
+#define CHAN_PARSER_HPP
 
-    #include "../base_parser.hpp"
-    #include "chan_post.hpp"
+#include "../base_parser.hpp"
+#include "chan_post.hpp"
 
-    struct chan_parser : public base_parser {
+struct chan_parser : public base_parser {
 
-    	std::string parse_postid(const pugi::xml_node &node);
+	std::string parse_postid(const pugi::xml_node &node);
 	std::string parse_post_text(const pugi::xml_node &node);
-    	std::string parse_post_img(const pugi::xml_node &node);
+	std::string parse_post_img(const pugi::xml_node &node);
 	std::string parse_post_img_name(const pugi::xml_node &node);
 
 	bool final_page(const std::string &xml);
 
 	std::vector<chan_post> parse_thread(const std::string &xml);
-    	std::string flatten(pugi::xml_node &&quote);
+	std::string flatten(pugi::xml_node &&quote);
 
 	std::vector<chan_post> parse_posts(const char*, 
 	const std::string&, pugi::xpath_node_set&&);
@@ -27,5 +27,5 @@
 		const std::string &xml);
 
 	chan_post parse_post(const char *board, const pugi::xml_node &node, const std::string &thread=std::string());
-    };
+};
 #endif
