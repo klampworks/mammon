1377ddf854bbe74ccffe7075c7e04d312bb49909
yukipyon <wolfvbear@gmail.com>
2014-01-01 11:51:20 +0000

    Added test for a known thread and its content.

--- chan/unit_test/chan_parser_unit_test.cpp
+++ chan/unit_test/chan_parser_unit_test.cpp
@@ -93,3 +93,55 @@ BOOST_AUTO_TEST_CASE(check_img_name) {
 
 	BOOST_CHECK(p.parse_post_img_name(doc) == expected);
 }
+
+
+BOOST_AUTO_TEST_CASE(check_parse_a_thread) {
+
+	chan_parser p;
+
+std::vector<chan_post> expected({
+{"test", "2374", "2374", "1356806391743.png", "http://desuchan.net/tech/src/1356806391743.png",
+"What are you working on?"},
+{"test", "2374", "2375", "1356867546959.jpg", "http://desuchan.net/tech/src/1356867546959.jpg", "NOTHING NOTHING AT ALL"},
+{"test", "2374", "2378", "", "", "A java fizzbuzz program"},
+{"test", "2374", "2400", "1369856779897.jpg", "http://desuchan.net/tech/src/1369856779897.jpg", ">>2378 Tryin` to make some shit in Python. Now it`s MAL parser. http://rghost.net/46363268"},
+{"test", "2374", "2404", "1372814015705.jpg", "http://desuchan.net/tech/src/1372814015705.jpg", ">>2374 nothing"},
+{"test", "2374", "2411", "1376735987000.jpg", "http://desuchan.net/tech/src/1376735987000.jpg", "figuring out how to make access with vba scripting take an exported excel file, convert it to a drop down table, and email it, just at the press of a button... because the people i work with are too stupid to attach a file and send an email themselves Last edited 13/08/17(Sat)06:39."},
+});
+
+	std::string xml = dump_file("thread_page.html");
+
+	std::vector<chan_post> ret = p.parse_thread("test", xml);
+
+	std::cout << ret.size() << " posts parsed." << std::endl;
+
+	for (unsigned i = 0; i < ret.size(); i++) {
+		BOOST_CHECK(ret[i].thread_id == expected[i].thread_id);
+		BOOST_CHECK(ret[i].post_id == expected[i].post_id);
+		BOOST_CHECK(ret[i].content == expected[i].content);
+		BOOST_CHECK(ret[i].img_url == expected[i].img_url);
+		BOOST_CHECK(ret[i].img == expected[i].img);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(check_parse_threads) {
+
+	chan_parser p;
+
+	std::string xml = dump_file("list_page.html");
+
+	std::vector<std::vector<chan_post>> ret = p.parse_threads("test", xml);
+
+	for (const auto &r : ret) {
+
+		for (const auto &a : r) {
+			std::cout << a.thread_id << std::endl;
+			std::cout << a.post_id << std::endl;
+			std::cout << a.img << std::endl;
+			std::cout << a.img_url << std::endl;
+			std::cout << a.content << std::endl;
+		}
+		std::cout << std::endl;
+	}
+
+}
