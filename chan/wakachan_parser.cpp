5828570621b65477780285d78646f6c0518edba0
yukipyon <wolfvbear@gmail.com>
2014-01-02 11:47:11 +0000

    Half baked solution to the no divs problem. Will porbably have to try again anyway.

--- chan/wakachan_parser.cpp
+++ chan/wakachan_parser.cpp
@@ -11,11 +11,53 @@ std::vector<chan_post> wakachan_parser::parse_a_thread(
 	return chan_parser::parse_a_thread("table/tbody/tr/td", board, node);
 }
 
+#include <iostream>
 std::vector<std::vector<chan_post>> wakachan_parser::parse_threads(
 	const char *board,
 	const std::string &xml)
 {
-	return chan_parser::parse_threads("//form[@id='delform']", board, xml);
+	pugi::xml_document doc;
+	doc.load(xml.c_str());
+	
+	const char *xpath = "//form[@id='delform']";
+
+	//Wakacahn does not wrap each thread in a <div>.
+	//Instead they separate each tread with a <hr>
+	auto subtree = doc.select_single_node(xpath);
+
+	std::vector<std::vector<chan_post>> threads;
+
+	auto st = subtree.node().begin();
+	auto en = subtree.node().end();
+
+	threads.push_back(std::vector<chan_post>());
+	unsigned index = 0;
+
+	for (;st != en; st++) {
+
+		//std::cout << st->name() << std::endl;
+		if (!strcmp(st->name(), "table")) {
+
+			auto node = st->select_single_node("tbody/tr/td[@class='reply']").node();
+
+			if (threads[index].empty())
+				threads[index].push_back(chan_parser::parse_post(board, node));
+			else 
+				threads[index].push_back(
+					chan_parser::parse_post(board, node, 
+						threads[index].front().thread_id));
+
+		} else if (!strcmp(st->name(), "hr")) {
+
+			threads.push_back(std::vector<chan_post>());
+			index++;	
+		}
+			
+
+	}
+
+	return threads;
+	//return chan_parser::parse_threads( board, xml);
 }
 
 bool wakachan_parser::final_page(const std::string &xml) 
