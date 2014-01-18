9ab001f7d02cb826ea94beb1c89bf0f7e1833724
yukipyon <wolfvbear@gmail.com>
2014-01-18 10:25:07 +0000

    Fixed incorrect url.

--- chan/wakachan_driver.cpp
+++ chan/wakachan_driver.cpp
@@ -3,7 +3,7 @@
 
 wakachan_driver::wakachan_driver(std::vector<std::string> &&boards_p) :
 	chan_driver("wakachan", new wakachan_parser(), 
-	std::move(boards_p), "http://wakachan.org/") {}
+	std::move(boards_p), "http://www.wakachan.org/") {}
 
 wakachan_driver::~wakachan_driver() 
 {
