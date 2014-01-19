3a78caa3a4f14068e9944db358b734d1203d91af
yukipyon <wolfvbear@gmail.com>
2014-01-19 13:55:56 +0000

    Fixed the final page logic.

--- chan/wakachan_parser.cpp
+++ chan/wakachan_parser.cpp
@@ -3,6 +3,9 @@
 #include "wakachan_parser.hpp"
 #include <vector>
 #include "chan_post.hpp"
+#include "../tidy.hpp"
+
+#include <iostream>
 
 std::vector<chan_post> wakachan_parser::parse_a_thread(
 	const char *board, 
@@ -15,21 +18,32 @@ std::vector<std::vector<chan_post>> wakachan_parser::parse_threads(
 	const char *board,
 	const std::string &xml)
 {
+	/* TODO rethink this. Actually the tidy should be elswhere anyway. */
+	std::string xml2 = xml;
+
+	/* The wakachan HTML source is beyond condemned. */
+	tidy::tidy(xml2);
+
 	pugi::xml_document doc;
-	doc.load(xml.c_str());
+	doc.load(xml2.c_str());
 	
 	const char *xpath = "//form[@id='delform']";
+	//const char *xpath = "/html/body/form[@id='delform']";
 
 	//Wakacahn does not wrap each thread in a <div>.
 	//Instead they separate each tread with a <hr>
 	auto subtree = doc.select_single_node(xpath);
 
+	std::cout << (bool)subtree << std::endl;
+
 	auto st = subtree.node().begin();
 	auto en = subtree.node().end();
 
 	//If we can't parse this then don't waste time with the other crap.
-	if (st == en)
+	if (st == en) {
+		std::cout << "Initial xpath failed." << std::endl;
 		return std::vector<std::vector<chan_post>>();
+	}
 
 	pugi::xml_document new_doc;
 	pugi::xml_node div;
@@ -50,7 +64,14 @@ START:
 	return chan_parser::parse_threads("div[@id='mammon']", board, new_doc);
 }
 
-bool wakachan_parser::final_page(const std::string &xml) 
+bool wakachan_parser::final_page(const std::string &input) 
 {
-	return chan_parser::final_page("(//form[@method='get' and @action])[last()]", xml);
+	std::string xml = input;
+	tidy::tidy(xml);
+
+	pugi::xml_document doc;
+	doc.load(xml.c_str());
+
+	const char *xpath = "//form[@method='get' and @action]/input[@value='Next']";
+	return !doc.select_single_node(xpath);
 }
