38fca3358f73ae64400a5aa5c7bdae68ef203086
yukipyon <wolfvbear@gmail.com>
2014-01-20 19:44:52 +0000

    Added Shanachan.

---
+++ chan/shanachan_driver.cpp
@@ -0,0 +1,30 @@
+#include "shanachan_driver.hpp"
+#include "wakachan_parser.hpp"
+#include "../kyukon/kyukon.hpp"
+
+shanachan_driver::shanachan_driver(std::vector<std::string> &&boards_p) :
+	chan_driver("shanachan", new wakachan_parser(), 
+	std::move(boards_p), "http://shanachan.org/") {}
+
+shanachan_driver::~shanachan_driver() 
+{
+	delete parser;
+}
+
+/* TODO Duplicate code. */
+void shanachan_driver::grab_post_img(const chan_post &post, 
+	const std::string &referer) 
+{
+
+	//Not all posts have images.
+	if (post.img_url.empty())
+		return;
+
+	std::string url = base_url + post.img_url.substr(1);
+
+	chan_task *t = new chan_task(domain_id, url, referer, task::FILE, 
+		std::bind(&chan_driver::process_image, this, std::placeholders::_1), post.board);
+
+	t->set_priority(4);	
+	kyukon::add_task(t);
+}
---
+++ chan/shanachan_driver.hpp
@@ -0,0 +1,12 @@
+#ifndef SHANACHAN_DRIVER_HPP
+#define SHANACHAN_DRIVER_HPP
+
+#include "chan_driver.hpp"
+
+struct shanachan_driver : public chan_driver {
+
+	shanachan_driver(std::vector<std::string>&&);
+	~shanachan_driver();
+	void grab_post_img(const chan_post&, const std::string&);
+};
+#endif
