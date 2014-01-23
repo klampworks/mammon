151d5eedd0816925938b88af1bd3b25c677dd2d0
yukipyon <wolfvbear@gmail.com>
2014-01-23 18:50:21 +0000

    Added unit tests for shanachan.

---
+++ chan/unit_test/shanachan_parser_unit_test.cpp
@@ -0,0 +1,31 @@
+#define BOOST_TEST_MODULE example
+#include <boost/test/included/unit_test.hpp>
+#include "../wakachan_parser.hpp"
+#include "parser_common.hpp"
+
+BOOST_AUTO_TEST_CASE(check_parse_a_thread) 
+{
+	wakachan_parser p;
+	BOOST_CHECK(
+		parse_thread_page("shanachan_thread_page.html", &p, 5));
+
+}
+
+BOOST_AUTO_TEST_CASE(check_parse_threads) 
+{
+	wakachan_parser p;
+	BOOST_CHECK(
+		parse_list_page("shanachan_list_page.html", &p, 11));
+}
+
+BOOST_AUTO_TEST_CASE(check_not_final_page)
+{
+	wakachan_parser p;
+	BOOST_CHECK(!parse_final_page("shanachan_list_page.html", &p));
+}
+
+BOOST_AUTO_TEST_CASE(check_final_page)
+{
+	wakachan_parser p;
+	BOOST_CHECK(parse_final_page("shanachan_final_page.html", &p));
+}
