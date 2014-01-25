013be27adad3a1d231d48efa99580996bafa67d0
yukipyon <wolfvbear@gmail.com>
2014-01-25 13:30:45 +0000

    Fixed typo.

--- chan/secchan_driver.hpp
+++ chan/secchan_driver.hpp
@@ -3,10 +3,10 @@
 
 #include "chan_driver.hpp"
 
-struct secachan_driver : public chan_driver {
+struct secchan_driver : public chan_driver {
 
-	secachan_driver(std::vector<std::string>&&);
-	~secachan_driver();
+	secchan_driver(std::vector<std::string>&&);
+	~secchan_driver();
 	void grab_post_img(const chan_post&, const std::string&);
 };
 #endif
