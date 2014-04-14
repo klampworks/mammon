f1445a1c0595c5866e7518729c737f8c6d8b06d9
yukipyon <wolfvbear@gmail.com>
2014-04-14 19:38:35 +0100

    Added generic driver test.

---
+++ driver_test.cpp
@@ -0,0 +1,67 @@
+#include "kyukon/kyukon.hpp"
+#include <fstream>
+#include <signal.h>
+#include <iostream>
+
+#include "chan/desuchan_driver.hpp"
+#include "chan/shanachan_driver.hpp"
+#include "chan/secchan_driver.hpp"
+#include "chan/wakachan_driver.hpp"
+#include "reddit/reddit_driver.hpp"
+
+static base_driver *driver;
+
+void quit(int) 
+{
+	std::cout << "SIGINT recieved, finishing surrent operations"
+	"and shutting down the system." << std::endl;
+	driver->quit();
+}
+
+int main(int argc, char **argv) 
+{
+	assert(argc > 1);
+
+	std::ifstream ifs;
+	ifs.open("proxies");
+
+	assert(ifs.good());
+
+	std::vector<std::pair<std::string, bool>> proxies;
+	std::string tmp;
+
+	while(std::getline(ifs, tmp)) {
+		
+		assert(!tmp.empty());
+		proxies.push_back(std::pair<std::string, bool>(tmp, true));
+	}
+
+	assert(!proxies.empty());
+
+	kyukon::init(proxies);
+	ifs.close();
+
+	ifs.open("driver_boards");
+	std::vector<std::string> boards;
+
+	while(std::getline(ifs, tmp))
+		boards.push_back(tmp);
+
+	if (!strcmp(argv[1], "desu")) {
+		driver = new desuchan_driver(std::move(boards));
+	} else if (!strcmp(argv[1], "waka")) {
+		driver = new wakachan_driver(std::move(boards));
+	} else if (!strcmp(argv[1], "shana")) {
+		driver = new shanachan_driver(std::move(boards));
+	} else if (!strcmp(argv[1], "sec")) {
+		driver = new secchan_driver(std::move(boards));
+	} else if (!strcmp(argv[1], "reddit")) {
+		driver = new secchan_driver(std::move(boards));
+	} else {
+		printf("Invalid domain %s\n", argv[1]);
+		exit(1);
+	}
+
+	signal(SIGINT, &quit);
+	while(1);
+}
