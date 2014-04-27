557f4d3a18a34e221c8d228fee030429478c74ad
yukipyon <wolfvbear@gmail.com>
2014-04-27 15:52:56 +0100

    All 4chan parser unit tests pass.

--- chan/unit_test/fourchan_parser_unit_test.cpp
+++ chan/unit_test/fourchan_parser_unit_test.cpp
@@ -3,14 +3,12 @@
 #include "../fourchan_parser.hpp"
 #include "parser_common.hpp"
 
-#if 0
 BOOST_AUTO_TEST_CASE(check_parse_a_thread) 
 {
 	fourchan_parser p;
 	BOOST_CHECK(
-		parse_thread_page("fourchan_thread_page.html", &p, 5));
+		parse_thread_page("fourchan_thread_page.html", &p, 139));
 }
-#endif
 
 BOOST_AUTO_TEST_CASE(check_parse_threads) 
 {
@@ -18,7 +16,6 @@ BOOST_AUTO_TEST_CASE(check_parse_threads)
 	BOOST_CHECK(
 		parse_list_page("fourchan_list_page.html", &p, 16));
 }
-#if 0
 
 BOOST_AUTO_TEST_CASE(check_not_final_page)
 {
@@ -33,4 +30,3 @@ BOOST_AUTO_TEST_CASE(check_final_page)
 }
 
 
-#endif
