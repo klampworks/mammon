5f5e1e1ae890cc34175d7686a1451f99d37f1ce4
yukipyon <wolfvbear@gmail.com>
2014-10-04 12:56:41 +0100

    Added debug print feature.

--- chan/chan_post.hpp
+++ chan/chan_post.hpp
@@ -1,6 +1,7 @@
 #ifndef CHAN_POST_HPP
 #define CHAN_POST_HPP
 
+	#include <iostream>
 struct chan_post {
 
 	std::string
@@ -12,18 +13,46 @@ struct chan_post {
 		content;
 	
 	chan_post(){}
-	chan_post(const std::string &board_p, const std::string &thread_id_p,
-		const std::string &post_id_p, const std::string &img_p, 
-		const std::string &img_url_p, const std::string &content_p) :
-			board(board_p), thread_id(thread_id_p), post_id(post_id_p),
-			img(img_p), img_url(img_url_p), content(content_p) {}
 
-	chan_post(const std::string &board_p, const std::string &thread_id_p,
-		std::string &&post_id_p, std::string &&img_p, 
-		std::string &&img_url_p, std::string &&content_p) :
-			board(board_p), thread_id(thread_id_p), post_id(std::move(post_id_p)),
-			img(std::move(img_p)), img_url(std::move(img_url_p)), 
+	chan_post(
+		const std::string &board_p, 
+		const std::string &thread_id_p,
+		const std::string &post_id_p, 
+		const std::string &img_p, 
+		const std::string &img_url_p, 
+		const std::string &content_p) :
+			board(board_p), 
+			thread_id(thread_id_p), 
+			post_id(post_id_p),
+			img(img_p), img_url(img_url_p), 
+			content(content_p) {}
+
+	chan_post(
+		const std::string &board_p, 
+		const std::string &thread_id_p,
+		std::string &&post_id_p, 
+		std::string &&img_p, 
+		std::string &&img_url_p, 
+		std::string &&content_p) :
+			board(board_p), 
+			thread_id(thread_id_p), 
+			post_id(std::move(post_id_p)),
+			img(std::move(img_p)), 
+			img_url(std::move(img_url_p)), 
 			content(std::move(content_p)) {}
+
+	/* TODO ifdef debug. */
+	void print() const {
+		std::cout 
+			<< "Board = " << board << "\n"
+			<< "Thread_id = " << thread_id << "\n"
+			<< "Post_id = " << post_id << "\n"
+			<< "Img = " << img << "\n"
+			<< "Img_url = " << img_url << "\n"
+			<< "Content = " << content.substr(0, 160) << "\n"
+			<< std::endl;
+	}
+
 };
 
 #endif
