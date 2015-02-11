8d83ccdd39db8f831c679a7dff0e5288ff6e4d03
yukipyon <wolfvbear@gmail.com>
2015-02-11 22:15:14 +0000

    Added some halfway decent formatting to the xml tree output.

--- chan/xml_tools/xml_tool.cpp
+++ chan/xml_tools/xml_tool.cpp
@@ -1,16 +1,38 @@
 #include <libxml/HTMLparser.h>
 #include <libxml/HTMLtree.h>
 
-void print_element_names(xmlNode *a_node)
+void print_element_names(xmlNode *a_node, int indent)
 {
 	xmlNode *cur_node = NULL;
 
 	for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
 		if (cur_node->type == XML_ELEMENT_NODE) {
-			printf("node type: Element, name: %s\n", cur_node->name);
+            for (int i = 0; i < indent; ++i)
+                printf(".");
+
+			//printf("node type: Element, name: %s\n", cur_node->name);
+            if (xmlStrEqual(BAD_CAST "br", cur_node->name))
+                continue;
+
+            printf("<%s", cur_node->name);
+            for (auto a = cur_node->properties; a; a = a->next) {
+                printf(" ");
+                printf("%s", a->name);
+                for (auto b = a->children; b; b = b->next) {
+                    auto t = xmlNodeGetContent(b);
+                    printf("='%s'", t);
+                    free(t);
+                }
+            }
+            printf(">\n");
 		}
 
-		print_element_names(cur_node->children);
+		print_element_names(cur_node->children, indent+1);
+		if (cur_node->type != XML_TEXT_NODE && cur_node->name) {
+            for (int i = 0; i < indent; ++i)
+                printf(".");
+            printf("</%s>\n", cur_node->name);
+        }
 	}
 }
 
@@ -23,7 +45,7 @@ int parse_file(const char *f_doc)
 	}
 
 	xmlNode *doc_head = xmlDocGetRootElement(doc);
-    print_element_names(doc_head);
+    print_element_names(doc_head, 0);
 
 	xmlFree(doc_head);
 	return 0;
