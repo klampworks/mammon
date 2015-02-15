f36622b86ac2e1b69b124208629219c6bc862998
yukipyon <wolfvbear@gmail.com>
2015-02-15 12:23:45 +0000

    Integrated nokogiri into the proof of conecpt.

--- chan/ruby/test.cpp
+++ chan/ruby/test.cpp
@@ -5,13 +5,17 @@ std::string dorb(const char *name)
 {
     int status;
 
+    //rb_protect((VALUE(*)(VALUE))rb_require, rb_str_new2("/usr/bin/nokogiri"), &status);
+    
     rb_load_protect(rb_str_new2(name), 0, &status);
     if (status) {
         VALUE rbError = rb_funcall(rb_gv_get("$!"), rb_intern("message"), 0);
         std::cerr << StringValuePtr(rbError) << std::endl;
     };
     
-    VALUE result = rb_funcall(Qnil, rb_intern("hello"), 1, rb_str_new2("alice"));
+    VALUE result = rb_funcall(Qnil, rb_intern("parse"), 1, 
+        rb_str_new2("./test.html"));
+
     return std::string(StringValuePtr(result));
 }
 
