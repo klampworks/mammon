520dbe2e2d612abcea3abe54d827654781c25bff
yukipyon <wolfvbear@gmail.com>
2015-05-14 19:05:18 +0000

    Code for parsing interesting parts of a post.

--- json_api/fourchan_parser_json.cpp
+++ json_api/fourchan_parser_json.cpp
@@ -1,20 +1,58 @@
+#include "fourchan_parser_json.hpp"
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <sstream>
 
 #include <iostream>
 
-std::vector<std::string> parse_threads(const std::string &json)
+boost::property_tree::ptree parse_json(const std::string &json)
 {
     boost::property_tree::ptree pt;
-    std::stringstream ss; ss << json;
+    std::stringstream ss; 
+    ss << json;
     read_json(ss, pt);
 
-    //for (const auto &v : pt.get_child("page")) {
+    return pt;
+}
+
+/* Parse the thread ids from a threads.json page. */
+std::vector<std::string> parse_threads(const std::string &json)
+{
+    auto pt(parse_json(json));
+
+    std::vector<std::string> ret;
     for (const auto &v : pt) {
         for (const auto &w : v.second.get_child("threads")) {
-        //std::cout << w.first << std::endl;
-        std::cout << w.second.get<std::string>("no") << std::endl;
+            ret.push_back(w.second.get<std::string>("no"));
         }
     }
+
+    return ret;
+}
+
+template <typename T>
+std::string sget(T &pt, const char *node)
+{
+    try {
+        return pt.second.template get<std::string>(node);
+    } catch (...) {
+        return "";
+    }
+}
+
+std::vector<chan_post> parse_posts(const std::string &json)
+{
+    auto pt(parse_json(json));
+
+    std::vector<chan_post> ret;
+    for (const auto &v : pt.get_child("posts")) {
+        chan_post cp;
+        cp.id = v.second.get<std::string>("no");
+        std::cout << sget(v, "sub") << std::endl;
+        std::cout << sget(v, "com") << std::endl;
+        std::cout << sget(v, "tim") << std::endl;
+        std::cout << sget(v, "ext") << std::endl;
+    }
+
+    return ret;
 }
--- json_api/fourchan_parser_json.hpp
+++ json_api/fourchan_parser_json.hpp
@@ -2,4 +2,18 @@
 
 #include <string>
 #include <vector>
+
+struct chan_post {
+    std::string id;
+    std::string sub;
+    std::string com;
+    std::string filename;
+
+    void set_filename(std::string tim, std::string ext)
+    {
+        filename = tim + ext;
+    }
+};
+
 std::vector<std::string> parse_threads(const std::string &json);
+std::vector<chan_post> parse_posts(const std::string &json);
