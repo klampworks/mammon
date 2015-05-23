c0795148c7b2081f7c039219d1e8019b4cfdf95a
yukipyon <wolfvbear@gmail.com>
2015-05-23 14:29:24 +0100

    Added database support for multiresource chan posts.

--- chan/chan_db.hpp
+++ chan/chan_db.hpp
@@ -7,11 +7,13 @@
 
 namespace chan_db {
 
-	void init();
+	void init(std::string db_file = "test.sqlite");
 	void init_table(const char *table_name);
 	bool insert_post(const char*, chan_post post);
 	void insert_posts(const char*, std::vector<chan_post>&);
-	bool post_exists(const char *table_name, chan_post post);
+	bool post_exists(std::string table_name, chan_post post);
+
+	extern std::vector<std::string> columns;
 
 }//namespace
 
