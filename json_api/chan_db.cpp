93a334c431b3c35f669e63ed5cee4bfd1de13dab
yukipyon <wolfvbear@gmail.com>
2015-06-04 18:12:38 +0000

    Implemented database store and exists? methods.

--- json_api/chan_db.cpp
+++ json_api/chan_db.cpp
@@ -2,7 +2,6 @@
 #include "chan_post.hpp"
 #include "../base_db.hpp"
 
-
 chan_db::chan_db(const std::string &chan_name)
     : cname(chan_name)
 {
@@ -22,5 +21,31 @@ chan_db::chan_db(const std::string &chan_name)
         "filename"});
 }
 
-bool store_post(const chan_post&);
-bool post_exists(const chan_post&);
+bool chan_db::store_post(const chan_post &cp)
+{
+    if (post_exists(cp))
+        return true;
+
+	base_db::insert_row(p_table, {
+        cp.get_board(), 
+        cp.get_thread_id(), 
+        cp.get_id(), 
+        cp.get_sub(), 
+        cp.get_com()});
+
+    for (const auto &fn : cp.get_filenames())
+        base_db::insert_row(f_table, {cp.get_id(), fn});
+
+    return true;
+}
+
+bool chan_db::post_exists(const chan_post &cp)
+{
+    const std::string query = 
+        "select * from " + p_table + " where board=? and id=?";
+
+    const std::string res = base_db::lookup_single_value(
+        query.c_str(), std::vector<std::string>({cp.get_board(), cp.get_id()}));
+
+    return res.empty();
+}
--- json_api/chan_db.hpp
+++ json_api/chan_db.hpp
@@ -6,7 +6,8 @@ class chan_post;
 class chan_db {
 
     std::string cname, p_table, f_table;
-    chan_db(const std::string &db_file, const std::string &chan_name);
-    bool store_post(const chan_post&);
-    bool post_exists(const chan_post&);
-}
+    public:
+        chan_db(const std::string &chan_name);
+        bool store_post(const chan_post&);
+        bool post_exists(const chan_post&);
+};
