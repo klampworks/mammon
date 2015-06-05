24e9f88a2fc632ef72c967ac354437917cf720a2
yukipyon <wolfvbear@gmail.com>
2015-06-05 18:11:40 +0000

    Fleshed out database unit test.

--- json_api/unit_test/chan_db_ut.cpp
+++ json_api/unit_test/chan_db_ut.cpp
@@ -1,8 +1,15 @@
 #define BOOST_TEST_MODULE fourchan_parser_json_ut
 #include "../../chan/unit_test/but.hpp"
 #include "../chan_db.hpp"
+#include "../chan_post.hpp"
 
 BOOST_AUTO_TEST_CASE(helllo) 
 {
-    BOOST_CHECK(true);
+    chan_db db("test_chan"); 
+    chan_post cp("x", "666", "111", "hello wolrd", "I like puppies.", 
+        {"hello.jpg", "world.jpg"});
+
+    BOOST_CHECK(!db.post_exists(cp));
+    BOOST_CHECK(db.store_post(cp));
+    BOOST_CHECK(db.post_exists(cp));
 }
