df10e5127127515e8e5b2bb5ed471243117152a5
yukipyon <wolfvbear@gmail.com>
2015-06-06 18:09:01 +0000

    Missing commas in vector initialisation.

--- json_api/chan_db.cpp
+++ json_api/chan_db.cpp
@@ -9,15 +9,15 @@ chan_db::chan_db(const std::string &chan_name)
     f_table = chan_name + "_files";
 
     base_db::init(chan_name + ".sqlite");
-    base_db::format_table(p_table, {
-        "board"
-        "thread_id"
-        "id"
-        "sub"
+    base_db::init_table(p_table, {
+        "board",
+        "thread_id",
+        "id",
+        "sub",
         "com"});
 
     base_db::format_table(f_table, {
-        "id"
+        "id",
         "filename"});
 }
 
