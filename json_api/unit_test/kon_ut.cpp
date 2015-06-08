0050b28e71fb5dd75fa4f33077ec5bf23736c5d1
yukipyon <wolfvbear@gmail.com>
2015-06-08 18:16:48 +0000

    Proxy unit test.

--- json_api/unit_test/kon_ut.cpp
+++ json_api/unit_test/kon_ut.cpp
@@ -15,3 +15,16 @@ BOOST_AUTO_TEST_CASE(grab_wiki_page)
     const std::string gold("<title>Mammon - Wikipedia, the free encyclopedia</title>");
     BOOST_CHECK(t.get_data().find(gold) != std::string::npos);
 }
+
+BOOST_AUTO_TEST_CASE(grab_wiki_page_tor) 
+{
+	auto t = task(1, 
+        "https://en.wikipedia.org/w/index.php?title=Mammon&oldid=665006469", 
+        "666", task::STRING, nullptr);
+
+    kon k("127.0.0.1:9050", true);
+    k.grab(&t);
+
+    const std::string gold("<title>Mammon - Wikipedia, the free encyclopedia</title>");
+    BOOST_CHECK(t.get_data().find(gold) != std::string::npos);
+}
