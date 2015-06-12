8a6402b0eb97218965873ba24c091c9b7c19244a
yukipyon <wolfvbear@gmail.com>
2015-06-12 18:13:35 +0000

    Overview of what the driver should do.

--- json_api/fourchan_proc.cpp
+++ json_api/fourchan_proc.cpp
@@ -1,4 +1,6 @@
 #include "fourchan_proc.hpp"
+#include "kyukon/task.hpp"
+#include "kyukon/kon.hpp"
 
 std::string fourchan_proc::domain = "4cdn.org";
 std::string fourchan_proc::text_sub = "a";
@@ -21,3 +23,53 @@ std::string fourchan_proc::mk_board_url(const std::string b)
     return "http://" + text_sub + "." + domain + "/" + b + "/threads.json";
 }
 
+bool fourchan_proc::proc_board(const std::string board)
+{
+    auto board_url = fourchan_proc::mk_board_url(board);
+
+	auto t = task(1, 
+        board_url, 
+        "http://" + fourchan_proc::domain, task::STRING, nullptr);
+
+    k.grab(&t);
+
+    auto thread_ids = p.parse_threads(t.get_data());
+    if (thread_ids.empty()) {
+        //Bad thigns
+        //we should at least have the OP right?
+        //Check for 403/404
+    }
+
+    for (const auto &thread_id : thread_ids) {
+
+        auto thread_url = mk_thread_url(board, thread_id);
+        auto thread_task = task(1, 
+            thread_url, 
+            t.get_url(), task::STRING, nullptr);
+
+        k.grab(&thread_task);
+        auto posts = p.parse_posts(thread_task.get_data(), chan_post(board, thread_id));
+
+        for (const auto &post : posts) {
+            
+            if (db.post_exists(post))
+                continue;
+
+            for (const auto &filename : post.get_filenames()) {
+                auto file_url = fourchan_proc::mk_file_url(board, filename);
+                auto file_task = task(1, 
+                    file_url, 
+                    thread_task.get_url(), task::FILE, nullptr);
+                k.grab(&file_task);
+                //CHeck result
+            }
+
+            db.store_post(post);
+        }
+
+    }
+    
+    return true;
+}
+
+fourchan_proc::fourchan_proc() : k("127.0.0.1:9050", true), db("fourchan") {}
--- json_api/fourchan_proc.hpp
+++ json_api/fourchan_proc.hpp
@@ -1,11 +1,19 @@
 #pragma once
 #include <string>
+#include <vector>
+#include "fourchan_parser_json.hpp"
+#include "kyukon/kon.hpp"
+#include "chan_db.hpp"
 
 class fourchan_proc {
 
     static std::string domain;
     static std::string text_sub;
     static std::string file_sub;
+
+    fourchan_parser_json p;
+    kon k;
+    chan_db db;
 
     public:
         static std::string mk_file_url(const std::string b, const std::string filename);
@@ -14,4 +22,6 @@ class fourchan_proc {
 
         static std::string mk_board_url(const std::string b);
 
+        bool proc_board(const std::string board);
+        fourchan_proc();
 };
