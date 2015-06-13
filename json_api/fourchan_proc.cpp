3ad13f093a08c5343fbd3d1debe1db7f20c829e4
yukipyon <wolfvbear@gmail.com>
2015-06-13 18:19:06 +0000

    Added basic logging.

--- json_api/fourchan_proc.cpp
+++ json_api/fourchan_proc.cpp
@@ -24,9 +24,11 @@ std::string fourchan_proc::mk_board_url(const std::string b)
     return "http://" + text_sub + "." + domain + "/" + b + "/threads.json";
 }
 
+#include <iostream>
 bool fourchan_proc::proc_board(const std::string board)
 {
     auto board_url = fourchan_proc::mk_board_url(board);
+    std::cout << "Processing " << board_url << std::endl;
 
 	auto t = task(1, 
         board_url, 
@@ -36,6 +38,7 @@ bool fourchan_proc::proc_board(const std::string board)
 
     auto thread_ids = p.parse_threads(t.get_data());
     if (thread_ids.empty()) {
+        std::cout << "No thread ids!" << std::endl;
         //Bad thigns
         //we should at least have the OP right?
         //Check for 403/404
@@ -43,6 +46,7 @@ bool fourchan_proc::proc_board(const std::string board)
 
     for (const auto &thread_id : thread_ids) {
 
+        std::cout << "Processing thread " << thread_id << std::endl;
         auto thread_url = mk_thread_url(board, thread_id);
         auto thread_task = task(1, 
             thread_url, 
@@ -51,12 +55,20 @@ bool fourchan_proc::proc_board(const std::string board)
         k.grab(&thread_task);
         auto posts = p.parse_posts(thread_task.get_data(), chan_post(board, thread_id));
 
+        if (posts.empty()) {
+            std::cout << "No posts!" << std::endl;
+            std::cout << "Url = " << thread_url << std::endl;
+            std::cout << "Data = " << thread_task.get_data() << std::endl;
+        }
+
         for (const auto &post : posts) {
             
             if (db.post_exists(post))
                 continue;
 
             for (const auto &filename : post.get_filenames()) {
+
+                std::cout << "Porcessing file " << filename << std::endl;
                 auto file_url = fourchan_proc::mk_file_url(board, filename);
                 auto file_task = task(1, 
                     file_url, 
