e5408149688c99c7b71c6e64d3501caf8b943476
yukipyon <wolfvbear@gmail.com>
2015-07-05 18:16:36 +0000

    Sqlite C++ API example.

--- json_api/db/test.cpp
+++ json_api/db/test.cpp
@@ -1,14 +1,30 @@
-#include <sqlpp11/sqlpp11.h>
-#include <sqlpp11/custom_query.h>
-#include <sqlpp11/sqlite3/sqlite3.h>
-#include <sqlite3.h>
+#include <sqlite/connection.hpp>
+#include <sqlite/execute.hpp>
+#include <sqlite/query.hpp>
+#include <iostream>
 
-namespace sql = sqlpp::sqlite3;
-int main()
-{
-    sql::connection_config config;
-    config.path_to_database = "hello.sqlite3";
-    config.flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
-    config.debug = true;
-	sql::connection db(config);
+int main() {
+  try {
+
+    sqlite::connection con("test.db");
+    sqlite::execute(con, "CREATE TABLE test(id INTEGER PRIMARY KEY, name TEXT);", true);
+    sqlite::execute ins(con, "INSERT INTO test VALUES(?, ?);");
+
+    ins % sqlite::nil % "Hello";
+    ins();
+
+    sqlite::query q(con, "SELECT * FROM test;");
+    
+    boost::shared_ptr<sqlite::result> result = q.get_result();
+    while(result->next_row()) {
+      std::cout << "ID:   " << result->get_int(0) << "\n"
+                << "Name: " << result->get_string(1) << std::endl;
+    }
+
+  }
+  catch(std::exception const & e) {
+    std::cout << "An error occurred: " << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
 }
