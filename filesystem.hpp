f8cf8cfde16fd7e6356857ccbdeb6e7e81986f6f
yukipyon <wolfvbear@gmail.com>
2015-12-23 19:03:29 +0000

    Added file exists function.

--- filesystem.hpp
+++ filesystem.hpp
@@ -6,5 +6,5 @@ namespace fs {
     void append_slash(std::string &path);
     bool create_path(const std::string &path);
     void unique_fn(std::string &fn);
-
+    bool file_exists(const std::string &path);
 }
