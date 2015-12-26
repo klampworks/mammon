b217bd0d128f76c213b4c7d82c00aaa6f7fa8ee7
yukipyon <wolfvbear@gmail.com>
2015-12-26 19:11:08 +0000

    Added a move function.

--- filesystem.hpp
+++ filesystem.hpp
@@ -3,6 +3,7 @@
 
 namespace fs {
 
+    bool move(const std::string &from, const std::string &to);
     void append_slash(std::string &path);
     bool create_path(const std::string &path);
     void unique_fn(std::string &fn);
