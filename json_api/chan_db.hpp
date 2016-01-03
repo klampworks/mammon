a47958bfb351c33a7e9dafaae9e860235389018b
yukipyon <wolfvbear@gmail.com>
2016-01-03 19:11:06 +0000

    Added a find hash function.

--- json_api/chan_db.hpp
+++ json_api/chan_db.hpp
@@ -13,4 +13,5 @@ class chan_db {
             const std::string &filename, const std::string &md5);
         bool post_exists(const chan_post&);
         bool file_exists(const std::string &board, const std::string &filename);
+        bool file_hash_exists(const std::string &hash);
 };
