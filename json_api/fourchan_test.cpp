c01b7569330c01eecfa17b992b62141ac609fa54
yukipyon <wolfvbear@gmail.com>
2016-01-21 19:03:48 +0000

    Load an externally defined Scheme score function if it exists.

--- json_api/fourchan_test.cpp
+++ json_api/fourchan_test.cpp
@@ -10,6 +10,7 @@
 #include <map>
 
 #include <chibi/eval.h>
+#include "score.hpp"
 
 void process(std::unique_ptr<chan_proc> pc, std::vector<std::string> boards)
 {
@@ -41,14 +42,35 @@ int main(int argc, char **argv)
     sexp ctx = sexp_make_eval_context(NULL, NULL, NULL, 0, 0);
     sexp_load_standard_env(ctx, NULL, SEXP_SEVEN);
 
-    sexp_gc_var1(file_path);
-    sexp_gc_preserve1(ctx, file_path);
+    sexp_gc_var2(file_path, score);
+    sexp_gc_preserve2(ctx, file_path, score);
     file_path = sexp_c_string(ctx, "mammon.ss", -1);
     sexp_load(ctx, file_path, NULL);
 
+    score = sexp_eval_string(ctx, "score", -1, NULL);
+    if (sexp_procedurep(score)) {
+        score::ext_score = [&ctx](const char *t) {
+            int score = 0;
+            sexp_gc_var3(arg_sym, arg_val, ret);
+            sexp_gc_preserve3(ctx, arg_sym, arg_val, ret);
+
+            arg_val = sexp_c_string(ctx, t, -1);
+            arg_sym = sexp_intern(ctx, "arg", -1);
+            sexp_env_define(ctx, sexp_context_env(ctx), arg_sym, arg_val);
+            ret = sexp_eval_string(ctx, "(score arg)", -1, NULL);
+
+            if (sexp_integerp(ret)) {
+                score = sexp_unbox_fixnum(ret);
+            }
+
+            sexp_gc_release3(ctx);
+            return score;
+        };
+    }
+
     opts.at(argv[1])(std::vector<std::string>(argv+2, argv+argc));
 
-    sexp_gc_release1(ctx);
+    sexp_gc_release2(ctx);
     sexp_destroy_context(ctx);
     return 0;
 }
