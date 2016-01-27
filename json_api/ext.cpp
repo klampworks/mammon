3e52f7452323c70dc1cdc2f472ffc0969c898883
yukipyon <wolfvbear@gmail.com>
2016-01-27 19:08:11 +0000

    Extension stub.

---
+++ json_api/ext.cpp
@@ -0,0 +1,9 @@
+#include "ext.hpp"
+
+namespace {
+
+    void load_file(sexp ctx, const char *filename)
+    {
+
+    }
+}
---
+++ json_api/ext.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <chibi/eval.h>
+
+namespace ext {
+
+    void load_file(sexp ctx, const char *filename);
+}
