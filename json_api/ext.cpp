5080c8538b5a9f3f4edaeb2f6da5325338143d69
yukipyon <wolfvbear@gmail.com>
2016-01-29 19:09:34 +0000

    Added implementation for load file.

--- json_api/ext.cpp
+++ json_api/ext.cpp
@@ -1,9 +1,15 @@
 #include "ext.hpp"
+#include <chibi/eval.h>
 
 namespace ext {
 
     void load_file(sexp ctx, const char *filename)
     {
+        sexp_gc_var1(filename_ss);
+        sexp_gc_preserve1(ctx, filename_ss);
+        filename_ss = sexp_c_string(ctx, filename, -1);
+        sexp_load(ctx, filename_ss, NULL);
 
+        sexp_gc_release1(ctx);
     }
 }
