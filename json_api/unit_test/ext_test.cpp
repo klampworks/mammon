c9f458d9ee292661bd6e4207248b08ee4b6f787e
yukipyon <wolfvbear@gmail.com>
2016-01-30 19:07:36 +0000

    First unit test for loading nonexistant scripts.

--- json_api/unit_test/ext_test.cpp
+++ json_api/unit_test/ext_test.cpp
@@ -2,7 +2,15 @@
 #include "../../chan/unit_test/but.hpp"
 #include "../ext.hpp"
 
-BOOST_AUTO_TEST_CASE(first) 
+/* TODO It might be worth diving deeper into Chibi exception handling. */
+BOOST_AUTO_TEST_CASE(load_nonexistant_script)
 {
-    BOOST_CHECK(true);
+    sexp_gc_var1(ret);
+    sexp ctx = sexp_make_eval_context(NULL, NULL, NULL, 0, 0);
+    sexp_load_standard_env(ctx, NULL, SEXP_SEVEN);
+
+    ret = ext::load_file(ctx, "does_not_exist.ss");
+    BOOST_CHECK(sexp_exceptionp(ret));
+
+    sexp_gc_release1(ctx);
 }
