6a60399dcd3a32e94f7412661f6f9ae4beb9b66c
yukipyon <wolfvbear@gmail.com>
2016-01-31 19:17:46 +0000

    Updated ext unti test for new load behavior.

--- json_api/unit_test/ext_test.cpp
+++ json_api/unit_test/ext_test.cpp
@@ -6,20 +6,15 @@
 /* TODO It might be worth diving deeper into Chibi exception handling. */
 BOOST_AUTO_TEST_CASE(load_nonexistant_script)
 {
-    sexp_gc_var1(ret);
     sexp ctx = sexp_make_eval_context(NULL, NULL, NULL, 0, 0);
     sexp_load_standard_env(ctx, NULL, SEXP_SEVEN);
 
-    ret = ext::load_file(ctx, "does_not_exist.ss");
-    BOOST_CHECK(sexp_exceptionp(ret));
-
-    sexp_gc_release1(ctx);
+    bool ret = ext::load_file(ctx, "does_not_exist.ss");
+    BOOST_CHECK(!ret);
 }
 
 BOOST_AUTO_TEST_CASE(load_existing_script)
 {
-    sexp_gc_var1(ret);
-
     const char *fn = "blank.ss";
     std::ofstream ofs;
     ofs.open("blank.ss");
@@ -29,8 +24,6 @@ BOOST_AUTO_TEST_CASE(load_existing_script)
     sexp ctx = sexp_make_eval_context(NULL, NULL, NULL, 0, 0);
     sexp_load_standard_env(ctx, NULL, SEXP_SEVEN);
 
-    ret = ext::load_file(ctx, fn);
-    BOOST_CHECK(!sexp_exceptionp(ret));
-
-    sexp_gc_release1(ctx);
+    bool ret = ext::load_file(ctx, fn);
+    BOOST_CHECK(ret);
 }
