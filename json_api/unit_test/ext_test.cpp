b98083d902526ce2c2c8937092322a6ed4f2776d
yukipyon <wolfvbear@gmail.com>
2016-02-03 19:14:35 +0000

    Unit test showing how to call a Scheme function and get back an integer value.

--- json_api/unit_test/ext_test.cpp
+++ json_api/unit_test/ext_test.cpp
@@ -58,3 +58,28 @@ BOOST_AUTO_TEST_CASE(fun_is_defined)
     bool ret = ext::fun_is_defined(ctx, "real-function");
     BOOST_CHECK(ret);
 }
+
+BOOST_AUTO_TEST_CASE(call_fun)
+{
+    const char *fn = "blank.ss";
+    std::ofstream ofs;
+    ofs.open("blank.ss");
+    ofs << "(define (real-function a b c) 666)" << std::endl;
+    ofs.close();
+
+    sexp ctx = sexp_make_eval_context(NULL, NULL, NULL, 0, 0);
+    sexp_load_standard_env(ctx, NULL, SEXP_SEVEN);
+
+    ext::load_file(ctx, fn);
+
+    {
+        sexp_gc_var1(ret);
+        sexp_gc_preserve1(ctx, ret);
+        ret = sexp_eval_string(ctx, "(real-function 1 2 3)", -1, NULL);
+
+        BOOST_CHECK(sexp_integerp(ret));
+        BOOST_CHECK_EQUAL(666, sexp_unbox_fixnum(ret));
+
+        sexp_gc_release1(ctx);
+    }
+}
