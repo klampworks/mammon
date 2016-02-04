2dac998e96051f41cabb6ec542a82db7f9a5c8ff
yukipyon <wolfvbear@gmail.com>
2016-02-04 19:03:20 +0000

    Unit test to demonstrate how to call a Scheme function with arguments.

--- json_api/unit_test/ext_test.cpp
+++ json_api/unit_test/ext_test.cpp
@@ -83,3 +83,34 @@ BOOST_AUTO_TEST_CASE(call_fun)
         sexp_gc_release1(ctx);
     }
 }
+
+BOOST_AUTO_TEST_CASE(call_fun_with_arg)
+{
+    const char *fn = "blank.ss";
+    std::ofstream ofs;
+    ofs.open("blank.ss");
+    ofs << "(define (strlen s) (string-length s))" << std::endl;
+    ofs.close();
+
+    sexp ctx = sexp_make_eval_context(NULL, NULL, NULL, 0, 0);
+    sexp_load_standard_env(ctx, NULL, SEXP_SEVEN);
+
+    ext::load_file(ctx, fn);
+
+    {
+        sexp_gc_var3(arg_sym, arg_val, ret);
+        sexp_gc_preserve3(ctx, arg_sym, arg_val, ret);
+
+        const std::string msg("hello world");
+        arg_val = sexp_c_string(ctx, msg.c_str(), -1);
+        arg_sym = sexp_intern(ctx, "arg", -1);
+        sexp_env_define(ctx, sexp_context_env(ctx), arg_sym, arg_val);
+
+        ret = sexp_eval_string(ctx, "(strlen arg)", -1, NULL);
+
+        BOOST_CHECK(sexp_integerp(ret));
+        BOOST_CHECK_EQUAL(msg.length(), sexp_unbox_fixnum(ret));
+
+        sexp_gc_release3(ctx);
+    }
+}
