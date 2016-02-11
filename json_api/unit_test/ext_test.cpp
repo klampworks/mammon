02db3c561b00ad50f34cbec0b8f376ebb9c7517d
yukipyon <wolfvbear@gmail.com>
2016-02-11 19:00:21 +0000

    Negative unit test for get string.

--- json_api/unit_test/ext_test.cpp
+++ json_api/unit_test/ext_test.cpp
@@ -214,3 +214,19 @@ BOOST_AUTO_TEST_CASE(get_string_unicode)
     BOOST_CHECK_EQUAL("hello † world", ret);
 }
 
+BOOST_AUTO_TEST_CASE(get_string_invalid1)
+{
+    const char *fn = "blank.ss";
+    std::ofstream ofs;
+    ofs.open("blank.ss");
+    ofs << "(define msg 666)" << std::endl;
+    ofs.close();
+
+    sexp ctx = sexp_make_eval_context(NULL, NULL, NULL, 0, 0);
+    sexp_load_standard_env(ctx, NULL, SEXP_SEVEN);
+
+    ext::load_file(ctx, fn);
+    std::string ret = ext::config_get_str(ctx, "msg", "Invalid");
+    BOOST_CHECK_EQUAL("Invalid", ret);
+}
+
