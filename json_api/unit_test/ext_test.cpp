253eb6f8362536961d1c0aa936a08e24bd4975c1
yukipyon <wolfvbear@gmail.com>
2016-02-16 19:08:05 +0000

    Negatve test for empty list.

--- json_api/unit_test/ext_test.cpp
+++ json_api/unit_test/ext_test.cpp
@@ -267,3 +267,19 @@ BOOST_AUTO_TEST_CASE(get_list)
     BOOST_CHECK_EQUAL("c", ret[2]);
 }
 
+BOOST_AUTO_TEST_CASE(get_no_list)
+{
+    const char *fn = "blank.ss";
+    std::ofstream ofs;
+    ofs.open("blank.ss");
+    ofs << "(define xs '())" << std::endl;
+    ofs.close();
+
+    sexp ctx = sexp_make_eval_context(NULL, NULL, NULL, 0, 0);
+    sexp_load_standard_env(ctx, NULL, SEXP_SEVEN);
+
+    ext::load_file(ctx, fn);
+    std::vector<std::string> ret = ext::config_get_list(ctx, "xs");
+
+    BOOST_CHECK_EQUAL(0, ret.size());
+}
